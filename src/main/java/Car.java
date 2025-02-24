d63d157ec430dd61854722abd7fcf4759d9b9f74
Sadeesha LK <sadeeshaseneviratne@gmail.com>
2025-02-25 03:23:41 +0530

    Updated java src files

---
+++ src/main/java/Car.java
@@ -0,0 +1,10 @@
+package java;
+
+public class Car {
+    private String carId;
+    private String carModel;
+    private String carType;
+    private int manufactureYear;
+
+    // Constructor, getters, setters
+}
---
+++ src/main/java/ServiceRecord.java
@@ -0,0 +1,11 @@
+package java;
+
+public class ServiceRecord {
+    private String serviceId;
+    private String carId;
+    private String serviceDate;
+    private String serviceType;
+    private double serviceCost;
+
+    // Constructor, getters, setters
+}
